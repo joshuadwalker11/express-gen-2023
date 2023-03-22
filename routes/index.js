47b691f8a860829c2165ae5bc411635dc6e1b717
unknown <Joshua Walker>
2023-03-22 12:19:36 -0400

    updated

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Welcome to my example of learning git', name: `Joshua's` });
+});
+
+router.get('home/landing', function (req, res, next) {
+  res.render('index', { title: "page: home" });
 });
 
 module.exports = router;
